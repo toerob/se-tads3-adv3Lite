10fba09d1e6a435da7b35b803e66568b1363f1be
Tomas Öberg <tomaserikoberg@gmail.com>
2026-05-30 16:27:13 +0200

    Anpassa efter skillnader i dagens adv3lite

--- tester/run_coverage.sh
+++ tester/run_coverage.sh
@@ -1,3 +1,3 @@
 #bash coverage.sh /path/to/se-tads3/lib /path/to/se-tads3/tester
 #bash coverage.sh --verbose /path/to/se-tads3/lib /path/to/se-tads3/tester
-bash coverage.sh --zero ~/code/tads3/adv3LiteSwe/lib/swedish ~/code/tads3/adv3LiteSwe/tester
+bash coverage.sh --zero ~/repos/se-tads3-adv3Lite/lib/swedish ~/repos/se-tads3-adv3Lite/tester
